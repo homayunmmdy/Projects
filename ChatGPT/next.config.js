42f090eb9713dd243ca0f3cb373198c170b9bcdb
Homayoun <homayoun763@gmail.com>
2024-03-04 23:18:37 +0330

    Adding chatgpt

---
+++ ChatGPT/next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  reactStrictMode: false, // changed this to false
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'i.ibb.co',
+      'scontent.fotp8-1.fna.fbcdn.net',
+    ],
+    // Make ENV
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
