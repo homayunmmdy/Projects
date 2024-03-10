3b027908edd5a01cf3a5abfc13860b666cb5d735
Homayoun <homayoun763@gmail.com>
2024-03-10 23:54:40 +0330

    remove nehazt

--- starter/src/App.tsx
+++ starter/src/App.tsx
@@ -1,8 +1,19 @@
+import "./app.scss"
 
 const App = () => {
+
+  const Button = {
+    width: "30px",
+    height: "30px",
+    background: "green"
+  }
+
   return (
-    <div>
-      Starter
+    <div style={{color:"red", backgroundColor:"blue"}}>
+      <div className="box"> 
+      <p>Text</p>
+      <button style={Button}>Button</button>
+      </div>
     </div>
   )
 }
